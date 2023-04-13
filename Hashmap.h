c8b5cc1254234c362390769c258d7fc4bc8dcbf7
Tyler Chartrand <tyler@thechartrands.com>
2023-04-12 20:47:19 -0600

    Update

--- Hashmap.h
+++ Hashmap.h
@@ -1,24 +1,52 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
+#include <list>
 #include "HashmapInterface.h"
+using std::vector, std::list, std::string;
 
 class Hashmap : public HashmapInterface {
 private:
     // put any private data members or methods here
 
+    size_t currSize = INITIAL_BUCKETS;
+    vector<list<int>> mainMap;
+
+    // https://stackoverflow.com/questions/98153/whats-the-best-hashing-algorithm-to-use-on-a-stl-string-when-using-hash-map
+    size_t getHashedPos(const string &str, size_t seed = 0) const {
+        const char* s = str.c_str();
+        size_t hash = seed;
+        while (*s) {
+            hash = hash * 101  +  *s++;
+        }
+        return hash % currSize;
+    }
+
+
+
 public:
     Hashmap() {
         // implement your constructor here
+        mainMap = vector<list<int>>(10);
+
     }
 
     ~Hashmap() override {
         // implement your destructor here
+        clear();
     }
 
     void insert(std::string key, int value) override {
-        // implement insert here
+        size_t pos = getHashedPos(key);
+        auto it = mainMap[pos].begin();
+        for (auto const &i : mainMap[pos]) {
+            if (i == value) {
+                return; //
+            }
+        }
+        mainMap[pos].push_back(value);
+
     }
 
     bool contains(const std::string &key) const override {
@@ -49,3 +77,4 @@ public:
         // implement size here
     }
 };
+
---
+++ Pair.h
@@ -0,0 +1,5 @@
+//
+// Created by tychart on 4/12/23.
+//
+
+#pragma once
